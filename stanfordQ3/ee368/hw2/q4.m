ea6f1b93f36fe9c4047c4c09a5db0e2f81af7855
Robert Konrad <rkkonrad@gmail.com>
2015-04-12 23:57:23 -0700

    Update on q4

--- stanfordQ3/ee368/hw2/q4.m
+++ stanfordQ3/ee368/hw2/q4.m
@@ -21,12 +21,88 @@ numsamples = 100;
 subset = zeros(100*5,1,3);
 for i = 1:5
     % Subset of cones
-    [x y z] = size(training{i});
-    cones = training{i} .* training_mask{i};
-    subseti = randperm(x*y)';
-    img = reshape(training{i}, [], 1, 3);
-    subset( (i-1)*numsamples + 1 : i*numsamples, 1,:) = img(subseti(1:numsamples),1,:);
+    cones = training{1} .* uint8(repmat(training_mask{1}, [1,1,3]));
+    cones = reshape(cones, [], 1, 3);
+    cones(find( sum(cones,3) == 0), :) = [];
+    
+    subseti = randperm(length(cones))';
+    subset_cones( (i-1)*numsamples + 1 : i*numsamples, 1,:) = cones(subseti(1:numsamples),:);
+    
+    % Subset of non_cones
+    noncones = training{1} .* uint8(repmat(~training_mask{1}, [1,1,3]));
+    noncones = reshape(noncones, [], 1, 3);
+    noncones(find(sum(noncones,3) == 0), :) = [];
+    
+    subseti = randperm(length(noncones))';
+    subset_non( (i-1)*numsamples + 1 : i*numsamples, 1,:) = noncones(subseti(1:numsamples),:);
 end
 
-scatter3(subset(:,:,1), subset(:,:,2), subset(:,:,3),'r');
+figure; hold;
+scatter3(subset_cones(:,:,1), subset_cones(:,:,2), subset_cones(:,:,3),'r');
+scatter3(subset_non(:,:,1), subset_non(:,:,2), subset_non(:,:,3),'b');
+xlabel('R');
+ylabel('G');
+zlabel('B');
+view([0.5,-1,1]);
+GRID ON;
 
+% We can see that there is a pretty significant seperation between colors
+% found in the cones, and in colors where there are no cones. This is good,
+% because it will make image segmentation based on color work well.
+
+%% Question 4. Part B
+
+bins_cones = zeros(16,16,16);
+bins_non = zeros(16,16,16);
+
+for k = 1:5
+    
+    % Cone bins
+    cones = training{k} .* uint8(repmat(training_mask{k}, [1,1,3]));
+    cones = reshape(cones, [], 1, 3);
+    cones(find( sum(cones,3) == 0), :) = [];
+    
+    for i = 1:length(cones)
+        color = cones(i,:);
+        index = double(color(:))/16;
+        index = floor(index) + 1;
+        %indx = sub2ind(size(bins_cones), floor(index) + 1);
+        bins_cones(index(1), index(2), index(3)) = ...
+                             bins_cones(index(1), index(2), index(3)) + 1;
+    end
+    
+    % Non-cone bins
+    noncones = training{k} .* uint8(repmat(~training_mask{k}, [1,1,3]));
+    noncones = reshape(noncones, [], 1, 3);
+    noncones(find( sum(noncones,3) == 0), :) = [];
+    
+    for i = 1:length(noncones)
+        color = noncones(i,:);
+        index = double(color(:))/16;
+        index = floor(index) + 1;
+        bins_non(index(1), index(2), index(3)) = ...
+                             bins_non(index(1), index(2), index(3)) + 1;
+    end
+end
+
+bins = bins_cones > bins_non;
+numOfConeBins = sum(bins(:));
+display(['The fraction of bins belonging to the cone class is : ' num2str(numOfConeBins / (16*16*16))]);
+
+%% Question 4. Part C
+
+for k = 1:5
+    [x y z] = size(training{k});
+    img = training{k};
+    mask = zeros(x,y);
+    for i = 1:x
+        for j = 1:y
+            color = img(i,j,:);
+            index = double(color(:))/16;
+            index = floor(index) + 1;
+            mask(i,j) = bins(index(1), index(2), index(3));
+        end
+    end
+    figure;
+    imshow(mask);
+end
